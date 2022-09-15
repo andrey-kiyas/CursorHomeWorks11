cf29ba95e13d4049ff184748b846252c1829a8b3
ANDREY <andrey.kiyas@gmail.com>
2022-09-15 16:36:47 +0300

    PR01 "Homework 11"

---
+++ src/main/java/com/CursorHomeWorks11/Exception/ShopNotFoundException.java
@@ -0,0 +1,7 @@
+package com.CursorHomeWorks11.Exception;
+
+public class ShopNotFoundException extends RuntimeException {
+    public ShopNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/CursorHomeWorks11/Repository/ShopRepository.java
@@ -0,0 +1,7 @@
+package com.CursorHomeWorks11.Repository;
+
+import com.CursorHomeWorks11.Entity.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+}
